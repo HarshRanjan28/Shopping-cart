ada814825d08b884e53e29125d5731693799832b
HarshRanjan28 <64592351+HarshRanjan28@users.noreply.github.com>
2022-11-26 16:36:31 +0530

    Update ContextCart.js

--- src/components/ContextCart.js
+++ src/components/ContextCart.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import Items from "./Items";
 import { CartContext } from "./Cart";
 const ContextCart = () => {
